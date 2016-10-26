4dc16c0d6478d21250e6254a34a03011808fa3c0
Robert Ikeoka <rikeoka@zendesk.com>
2016-10-25 23:44:38 -0700

    Member hook test
    Update member hook handler
    Add test and fixture for member hook

--- hooks/member.js
+++ hooks/member.js
@@ -1,15 +1,15 @@
 module.exports = function(payload) {
   // Flattens the payload object to prevent null method errors if payload isn't complete
   return {
-    'type': 'member',
-    'action': payload['action'],
-    'member': payload['member.login'],
-    'member_id': payload['member.id'],
-    'time': new Date(),
-    'repository_id': payload['repository.id'],
-    'repository': payload['repository.full_name'],
-    'repository_private': payload['repository.private'],
-    'sender': payload['sender.login'],
-    'sender_id': payload['sender.id']
+    type: 'member',
+    action: payload['action'],
+    member: payload['member.login'],
+    member_id: payload['member.id'],
+    time: new Date(),
+    repository_id: payload['repository.id'],
+    repository: payload['repository.full_name'],
+    repository_private: payload['repository.private'],
+    sender: payload['sender.login'],
+    sender_id: payload['sender.id']
   };
 };
