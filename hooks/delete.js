df0a52064a5d67644e5fc8fd24612391582f479a
Robert Ikeoka <rikeoka@zendesk.com>
2016-10-25 22:05:11 -0700

    Delete Hook tests
    Update delete hook handler
    Add test and fixture for delete hook

--- hooks/delete.js
+++ hooks/delete.js
@@ -1,14 +1,14 @@
 module.exports = function(payload) {
   // Flattens the payload object to prevent null method errors if payload isn't complete
   return {
-    'type': 'delete',
-    'ref': payload['ref'],
-    'ref_type': payload['ref_type'],
-    'time': new Date(),
-    'repository_id': payload['repository.id'],
-    'repository': payload['repository.full_name'],
-    'repository_private': payload['repository.private'],
-    'sender': payload['sender.login'],
-    'sender_id': payload['sender.id']
+    type: 'delete',
+    ref: payload['ref'],
+    ref_type: payload['ref_type'],
+    time: new Date(),
+    repository_id: payload['repository.id'],
+    repository: payload['repository.full_name'],
+    repository_private: payload['repository.private'],
+    sender: payload['sender.login'],
+    sender_id: payload['sender.id']
   };
 };
