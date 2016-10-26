c78e798964330f81a16987280b91d9f0c00cadeb
Robert Ikeoka <rikeoka@zendesk.com>
2016-10-25 21:50:58 -0700

    Commit Comment Test
    Update commit_comment hook handler
    Add test for commit_comment hook

--- hooks/commit_comment.js
+++ hooks/commit_comment.js
@@ -1,23 +1,22 @@
 module.exports = function(payload) {
   // Flattens the payload object to prevent null method errors if payload isn't complete
   return {
-    'type': 'commitCommentEvent',
-    'action': payload['action'],
-    'commit_id': payload['comment.commit_id'],
-    'comment_position': payload['comment.position'],
-    'comment_line': payload['comment.line'],
-    'comment_path': payload['comment.path'],
-    'comment_id': payload['comment.comment_id'],
-    'comment_body': payload['comment.body'],
-    'comment_author': payload['comment.user.login'],
-    'comment_author_id': payload['comment.user.id'],
-    'created': payload['comment.created_at'],
-    'updated': payload['comment.updated_at'],
-    'time': new Date(),
-    'repository_id': payload['repository.id'],
-    'repository': payload['repository.full_name'],
-    'repository_private': payload['repository.private'],
-    'sender': payload['sender.login'],
-    'sender_id': payload['sender.id']
+    type: 'commitCommentEvent',
+    action: payload['action'],
+    commit_id: payload['comment.commit_id'],
+    comment_position: payload['comment.position'],
+    comment_line: payload['comment.line'],
+    comment_path: payload['comment.path'],
+    comment_id: payload['comment.id'],
+    comment_author: payload['comment.user.login'],
+    comment_author_id: payload['comment.user.id'],
+    created: payload['comment.created_at'],
+    updated: payload['comment.updated_at'],
+    time: new Date(),
+    repository_id: payload['repository.id'],
+    repository: payload['repository.full_name'],
+    repository_private: payload['repository.private'],
+    sender: payload['sender.login'],
+    sender_id: payload['sender.id']
   };
 };
