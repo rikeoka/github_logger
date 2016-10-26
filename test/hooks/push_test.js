a1756b9fa782f2899cac42533600e350d256a0ed
Robert Ikeoka <rikeoka@zendesk.com>
2016-10-25 23:18:42 -0700

    Push hook test
    Update push hook handler
    Add test and fixture for push hook

---
+++ test/hooks/push_test.js
@@ -0,0 +1,29 @@
+// common includes
+var flatten = require('../../utils/flatten');
+var should = require('chai').should();
+
+// handler specific includes
+var handler = require('../../hooks/push');
+var payload = require('../fixtures/push.json');
+
+describe('push', function () {
+  it('returns expected keys', function () {
+    var response = handler({});
+    response.should.have.all.keys(payload.clean);
+  });
+
+  it('processes payload file', function() {
+    var contents = flatten(payload.payload);
+    var response = handler(contents);
+    var diff = (new Date()) - response['time'];
+    delete(response['time']);
+    delete(payload.clean.time);
+    diff.should.be.lt(2 * 1000);
+    response.should.be.eql(payload.clean);
+  });
+
+  it('completes on empty object', function () {
+    handler({}).should.be.an('object');
+    handler({}).should.not.Throw;
+  });
+});
