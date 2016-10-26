a25da8f98671b21ddff6c17ba41cdf3ec1e5deaf
Robert Ikeoka <rikeoka@zendesk.com>
2016-10-25 22:41:03 -0700

    Gollum hook Test
    Update Gollum Hook handler
    Add test and fixtures for gollum hook.

--- hooks/gollum.js
+++ hooks/gollum.js
@@ -1,13 +1,12 @@
 module.exports = function(payload) {
   // Flattens the payload object to prevent null method errors if payload isn't complete
   return {
-    'type': 'gollum (wiki)',
-    'pages': payload['pages'],
-    'time': new Date(),
-    'repository_id': payload['repository.id'],
-    'repository': payload['repository.full_name'],
-    'repository_private': payload['repository.private'],
-    'sender': payload['sender.login'],
-    'sender_id': payload['sender.id']
+    type: 'gollum - wiki',
+    time: new Date(),
+    repository_id: payload['repository.id'],
+    repository: payload['repository.full_name'],
+    repository_private: payload['repository.private'],
+    sender: payload['sender.login'],
+    sender_id: payload['sender.id']
   };
 };
