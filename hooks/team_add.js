a6465f3d4302087276ff3634fb8b438b5b512023
Robert Ikeoka <rikeoka@zendesk.com>
2016-10-25 22:55:47 -0700

    Team Add Hook Tests
    Update team_add hook handler
    Add test and fixture for team_add hook

--- hooks/team_add.js
+++ hooks/team_add.js
@@ -1,15 +1,15 @@
 module.exports = function(payload) {
   // Flattens the payload object to prevent null method errors if payload isn't complete
   return {
-    'type': 'team_add',
-    'team': payload['team.slug'],
-    'team_id': payload['team.id'],
-    'team_name': payload['team.name'],
-    'time': new Date(),
-    'repository_id': payload['repository.id'],
-    'repository': payload['repository.full_name'],
-    'repository_private': payload['repository.private'],
-    'sender': payload['sender.login'],
-    'sender_id': payload['sender.id']
+    type: 'team_add',
+    team: payload['team.slug'],
+    team_id: payload['team.id'],
+    team_name: payload['team.name'],
+    time: new Date(),
+    repository_id: payload['repository.id'],
+    repository: payload['repository.full_name'],
+    repository_private: payload['repository.private'],
+    sender: payload['sender.login'],
+    sender_id: payload['sender.id']
   };
 };
