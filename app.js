33393fef8431dd6860a212b16088611a14ef8fb8
Robert Ikeoka <rikeoka@zendesk.com>
2016-11-01 08:28:25 -0700

    Get HMAC to work
    hmac-express package doesn’t work due to formatted JSON being provided
    by Github.
    
    To get HMAC to work, needed to read the IO stream of the request to get
    the raw contents perform HMAC validation, and also populate the
    req.body JSON object.
    
    Because the HMAC validation already had read the contents of the stream
    bodyParser.json can’t be used.

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var SplunkLogger = require('splunk-logging').Logger;
+var crypto = require('crypto');
+var bufferEq = require('buffer-equal-constant-time');
 
-var hmacValidation = require('hmac-express')('sha1', process.env['HMAC_SECRET'], '', {header: 'HTTP_X_HUB_SIGNATURE'});
 var flatten = require('./utils/flatten');
 var events = require('./hooks/events');
 
 var app = express();
-var jsonParser = bodyParser.json();
-
 if (!!process.env['HMAC_SECRET']) {
-  app.post('/', hmacValidation, jsonParser, processWebhook);
+  app.post('/', hmacV, processWebhook);
 } else {
-  app.post('/', jsonParser, processWebhook);
+  app.post('/', bodyParser.json(), processWebhook);
 }
 
 if (!!process.env['SPLUNK_TOKEN'] && !!process.env['SPLUNK_URL']) {
@@ -32,7 +31,7 @@ app.listen(process.env.PORT || 3000, function() {
 function processWebhook(req, res) {
   payload = flatten(req.body);
 
-  var githubEvent = req.get('X-GitHub-Event');
+  var githubEvent = req.get('x-gitHub-event');
   if (events.eventTypes.includes(githubEvent)){
     event = events[githubEvent](payload);
   } else {
@@ -40,4 +39,28 @@ function processWebhook(req, res) {
   }
   Logger.log(event);
   res.sendStatus(204);
+}
+
+function hmacV(req, res, next) {
+  var payload = '';
+
+  req.on('data', function(chunk) {
+    payload += chunk;
+  });
+
+  req.on('end', function() {
+    var calculated = new Buffer(signature(process.env['HMAC_SECRET'], payload));
+    var provided = new Buffer(req.get('x-hub-signature'));
+
+    if (bufferEq(calculated, provided)) {
+      req.body = JSON.parse(payload);
+      next();
+    } else {
+      return res.sendStatus(401);
+    }
+  });
+}
+
+function signature(secret, body) {
+  return 'sha1=' + crypto.createHmac('sha1', secret).update(body).digest('hex');
 }
